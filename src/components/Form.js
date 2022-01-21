22070430709def2d42372f9fe0d7e530c9129606
radugeorge412 <radugeorge412@gmail.com>
2022-01-21 20:59:38 +0200

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Form({ input, setInput, todos, setTodos }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newState = [
+      ...todos,
+      { text: input, id: Math.floor(Math.random() * 100000), completed: false },
+    ];
+    setTodos(newState);
+    setInput("");
+  };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>What are we doing today?</h1>
+      <form className="todo-form" autoComplete="off">
+        <input
+          type="text"
+          placeholder="Add a todo"
+          name="text"
+          className="todo-input"
+          value={input}
+          onChange={handleChange}
+        />
+        <button className="todo-button" onClick={handleSubmit}>
+          Add todo
+        </button>
+      </form>
+    </>
+  );
+}
---
+++ src/components/List.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function List() {
+  return (
+    <div className="todoContainer">
+      <ul className="todoList"></ul>
+    </div>
+  );
+}
